02341b53501958240814b530fcf941b29bac0d62
Ben Jones <benwjones76@outlook.com>
2020-10-13 15:47:07 +0100

    Challenge 10 complete

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,14 @@
 require 'docking_station'
 
 RSpec.describe DockingStation do
-	it {respond_to(:release_bike)}	
+	it {respond_to(:release_bike)}
+
+	it "releases a bike" do
+		bike = subject.release_bike
+		expect(bike).to be_instance_of Bike
+	end
+	it "bike is working" do
+		bike = subject.release_bike
+		expect(bike.working?).to eq true
+	end
 end
