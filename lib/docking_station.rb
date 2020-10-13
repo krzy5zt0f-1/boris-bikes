7ac188529cc7a5a2ed5a23556c4a5f4cea7a37c9
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-10-13 16:58:25 +0100

    challenge 11 complete

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,16 @@
+require_relative './bike'
+
 class DockingStation
+	def initialize
+		@array_of_bikes = []
+	end
 	def release_bike
 		Bike.new
+	end
+	def dock(input)
+		@array_of_bikes.push(input)
+	end
+	def show_bikes
+		@array_of_bikes
 	end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
-require 'docking_station'
+require_relative '../lib/docking_station'
+
 
 RSpec.describe DockingStation do
 	it {respond_to(:release_bike)}
@@ -10,5 +11,26 @@ RSpec.describe DockingStation do
 	it "bike is working" do
 		bike = subject.release_bike
 		expect(bike.working?).to eq true
+	end
+	describe '#docking' do
+		it "docks a bike to be returned" do
+			# Arrange
+			bike = subject.release_bike
+			bikes = subject.instance_variable_get("@array_of_bikes")
+			#Act
+			subject.dock(bike)
+			#Assert
+			expect(bikes).to eq [bike]
+		end
+	end
+	describe '#show_bikes' do
+		it "returns non-empty array of bikes" do
+			# Arrange
+			bike = subject.release_bike
+			#Act
+			subject.dock(bike)
+			#Assert
+			expect(subject.show_bikes.size).to eq 1
+		end
 	end
 end
