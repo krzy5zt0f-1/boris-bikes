d76cfa44fda49b663c84816c06305460f078b3df
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-10-13 15:22:18 +0100

    challenge 9 complete

---
+++ spec/bike_spec.rb
@@ -0,0 +1,5 @@
+require 'bike'
+
+RSpec.describe Bike do
+	it {respond_to(:working?)}	
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,5 @@
 require 'docking_station'
 
 RSpec.describe DockingStation do
-	it "responds to #release bike" do
-		expect(subject).to respond_to(:release_bike)
-	end
+	it {respond_to(:release_bike)}	
 end
