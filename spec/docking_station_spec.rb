3b8ad8fbd357747dfe941e6d56ed7b0f87eb20b8
Ben Jones <benwjones76@outlook.com>
2020-10-14 20:09:40 +0100

    Challenge 20 complete

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,10 @@
 require_relative '../lib/docking_station'
 
 RSpec.describe DockingStation do
+#let(:bike) {double( :bike, :bike_condition => "broken")}
+let(:bike) {double(:bike, :working? => true, :bike_condition => "working")}
+let(:broken_bike) {double(:broken_bike, :working? => false, :bike_condition => "broken")}
+
 	it {is_expected.to respond_to(:release_bike)}
 
 	it "errors in no bike is available" do
@@ -8,26 +12,25 @@ RSpec.describe DockingStation do
 	end
 
 	it "releases a bike if one is available" do
-		bike = Bike.new
 		subject.dock(bike)
 		expect(subject.release_bike).to eq bike
 	end
 
 	it "bike is working" do
-		bike = Bike.new
+		allow(bike).to receive(:working?).and_return(true)
 		expect(bike.working?).to eq true
 	end
 
-	it "docks a broken bike and holds on to" do
-		bike = Bike.new
-		subject.dock(bike, "broken")
+	it "docks a broken bike and holds on to it" do
+		allow(broken_bike).to receive(:bike_condition=)
+		allow(broken_bike).to receive(:bike_condition).and_return("broken")
+		subject.dock(broken_bike, "broken")
 		expect{subject.release_bike}.to raise_error "Error: No working bikes available"
 	end
 
 	describe '#docking' do
 		it "docks a bike to be returned when dock is empty and no condition is given" do
 			# Arrange
-			bike = Bike.new
 			bikes = subject.instance_variable_get("@array_of_bikes")
 			#Act
 			subject.dock(bike)
@@ -36,22 +39,21 @@ RSpec.describe DockingStation do
 		end
 
 		it "errors when docking a bike ot a full dock" do
-			subject.DEFAULT_CAPACITY.times {subject.dock(Bike.new)}
-			expect{subject.dock(Bike.new)}.to raise_error "Docking station at maximum capacity"
+			subject.DEFAULT_CAPACITY.times {subject.dock(bike)}
+			expect{subject.dock(bike)}.to raise_error "Docking station at maximum capacity"
 		end
 
 		it "stores the condition of the bike when broken" do
-			bike = Bike.new
-			subject.dock(bike, "broken")
-			expect(bike.bike_condition).to eq "broken"
+			allow(broken_bike).to receive(:bike_condition=)
+			allow(broken_bike).to receive(:bike_condition).and_return("broken")
+			subject.dock(broken_bike, "broken")
+			expect(broken_bike.bike_condition).to eq "broken"
 		end
 	end
 
 	describe '#show_bikes' do
 		it "returns non-empty array of bikes" do
-			# Arrange
-			bike = Bike.new
-			#Act
+
 			subject.dock(bike)
 			#Assert
 			expect(subject.show_bikes.size).to eq 1
